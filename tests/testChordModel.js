a3d079fd9bb3316d1a06999d76aa5748c3731d97
Timotee Neullas <neullas@csl.sony.fr>
2014-10-08 17:34:30 +0200

    ChordModel refactored

--- tests/testChordModel.js
+++ tests/testChordModel.js
@@ -3,10 +3,53 @@ define(['modules/core/ChordModel'], function(ChordModel) {
 		run: function() {
 			test("Chords", function(assert) {
 				var chord = new ChordModel();
+				
+				// Basic Test
 				assert.equal(chord.toString(), "");
-				assert.throws(function(){
+				assert.throws(function() {
 					chord.setNote();
 				});
+				assert.equal(chord.getNote(), "");
+				assert.equal(chord.getChordType(), "");
+				assert.ok(!chord.getParenthesis());
+				assert.deepEqual(chord.getBase(), {});
+
+				// Adding base chord
+				var chordBase = new ChordModel();
+				chordBase.setNote('G');
+				chordBase.setChordType('m');
+				chord.setBase(chordBase);
+				assert.equal(chord.getBase().getNote(), "G");
+				assert.equal(chord.getBase().getChordType(), "m");
+				// Remove base chord
+				chord.setBase("");
+				
+				// Accidentals
+				chord.setNote("Db");
+				assert.equal(chord.getNote(), "Db");
+
+				// ChordTypes
+				chord.setChordType("M7");
+				assert.equal(chord.getChordType(), "M7");
+				
+				assert.throws(function() {
+					chord.setParenthesis();
+				});
+
+				// testing to String
+				assert.equal(chord.toString(), "DbM7");
+				assert.equal(chord.toString(" "), "Db M7");
+				chord.setParenthesis(true);
+
+				assert.equal(chord.toString(""), "(DbM7)");
+
+				// setChordFromString
+				chord.setChordFromString('A#m9/E');
+				assert.equal(chord.getNote(), "A#");
+				assert.equal(chord.getChordType(), "m9");
+				assert.equal(chord.getBase().getNote(), "E");
+				assert.equal(chord.getBase().getChordType(), "");
+
 			});
 		}
 	}
