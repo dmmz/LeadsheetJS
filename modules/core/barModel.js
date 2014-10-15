6cb64c48e8935b733dd3d64f08d3185e037b274b
Timotee Neullas <neullas@csl.sony.fr>
2014-10-15 14:16:48 +0200

    Remove import cslJson from barmodel

--- modules/core/barModel.js
+++ modules/core/barModel.js
@@ -116,38 +116,5 @@ define(function() {
 		return new BarModel(this);
 	};
 
-	BarModel.prototype.importFromMusicCSLJSON = function(JSONBar) {
-		var self = this;
-		var labels = ["segno", "segno2", "fine", "coda", "coda2", "on cue"];
-		labels.forEach(function(label) {
-			if (JSONBar.hasOwnProperty(label)) {
-				self.setLabel(label);
-			}
-		});
-		//so far now we allow only one label per bar in the DB, but this code is prepared to allow more than one, as an array
-		if (JSONBar.hasOwnProperty('ending')) self.setEnding(JSONBar.ending);
-		if (JSONBar.hasOwnProperty('sublabel')) self.setSublabel(JSONBar.sublabel);
-		if (JSONBar.hasOwnProperty('timeSignature')) self.setTimeSignature(JSONBar.timeSignature);
-
-	};
-	BarModel.prototype.exportToMusicCSLJSON = function() {
-		var bar = {};
-
-		if (this.getLabel())
-			bar[this.getLabel()] = 1;
-
-		if (this.getEnding())
-			bar.ending = this.getEnding();
-
-		if (this.getSublabel())
-			bar.sublabel = this.getSublabel();
-
-		if (this.getTimeSignature())
-			bar.timeSignature = this.getTimeSignature();
-
-		return bar;
-
-	};
-
 	return BarModel;
 });
