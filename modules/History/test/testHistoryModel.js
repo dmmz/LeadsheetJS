ced7044154284f1e0a35b7021b8601a1f8c9037c
Dani <dmartinmartinez@gmail.com>
2015-12-03 18:56:45 +0100

    working history, fixed bugs when rewriting history and update last entry. Also, refactored

--- modules/History/test/testHistoryModel.js
+++ modules/History/test/testHistoryModel.js
@@ -11,44 +11,113 @@ define([
 				assert.equal(hm.historyList.length, 0, 'on initialisation, no history');
 				assert.equal(hm.getCurrentPosition(), -1);
 				assert.equal(hm.lastLeadsheet, null);
-				
+
 				//on load we controller will do first add to history, initial lead sheet (position -1)
-				hm.addToHistory({title:'ls'}, 'initial leadsheet');
+				hm.addToHistory({
+					title: 'ls'
+				}, 'initial leadsheet');
 				assert.equal(hm.getCurrentPosition(), 0, 'tests initial lead sheet');
 				assert.equal(hm.historyList.length, 1);
-				assert.deepEqual(hm.lastLeadsheet, {title:'ls'});
+				assert.deepEqual(hm.lastLeadsheet, {
+					title: 'ls'
+				});
 
-				
-				//adding 1st change
-				hm.addToHistory({title:'ls', composer:'hey'}, '1st change');
+
+				hm.addToHistory({
+					title: 'ls',
+					composer: 'hey'
+				}, '1st change');
+
 				assert.equal(hm.getCurrentPosition(), 1, 'tests 1st change');
-				assert.deepEqual(hm.lastLeadsheet, {title:'ls', composer:'hey'});
+				assert.deepEqual(hm.lastLeadsheet, {
+					title: 'ls',
+					composer: 'hey'
+				});
 
-				hm.addToHistory({title:'ls', composer:'hey'}, '2nd change with no changes');
+				hm.addToHistory({
+					title: 'ls',
+					composer: 'hey'
+				}, '2nd change with no changes');
+
 				assert.equal(hm.getCurrentPosition(), 1, 'position has not moved because no changes were added');
 
-				hm.addToHistory({title:'ls', composer:'hi', source:'real book'}, '2nd change, now this is a good one');
+				hm.addToHistory({
+						title: 'ls',
+						composer: 'hi',
+						source: 'real book'
+					},
+					'2nd change, now this is a good one');
+
 				assert.equal(hm.getCurrentPosition(), 2, 'tests 2nd change');
 
-				hm.addToHistory({title:'ls', composer:'hi'}, '3rd change, removed source');
+				hm.addToHistory({
+					title: 'ls',
+					composer: 'hi'
+				}, '3rd change, removed source');
+
 				assert.equal(hm.getCurrentPosition(), 3, 'tests 3rd change');
-				
-				
-				hm.setCurrentPosition(hm.getCurrentPosition() - 1);
-				assert.equal(hm.getCurrentPosition(), 2);
-				assert.deepEqual(hm.getCurrentState(), {title:'ls', composer:'hi', source:'real book'}, 'going to position 1');
-				
-				hm.setCurrentPosition(hm.getCurrentPosition() - 1);
-				assert.equal(hm.getCurrentPosition(), 1, 'going to position 1');
-				assert.deepEqual(hm.getCurrentState(), {title:'ls', composer:'hey'});
+
+				hm.addToHistory({
+					title: 'ls',
+					composer: 'huu'
+				}, 'new changing, but updating', true);
+
+				assert.equal(hm.getCurrentPosition(), 3, 'tests change updating');
 
 				hm.setCurrentPosition(hm.getCurrentPosition() - 1);
-				assert.equal(hm.getCurrentPosition(), 0,'going to position -1');
-				assert.deepEqual(hm.getCurrentState(), {title:'ls'});
-				
+				assert.equal(hm.getCurrentPosition(), 2);
+				assert.deepEqual(hm.getCurrentState(), {
+					title: 'ls',
+					composer: 'hi',
+					source: 'real book'
+				}, 'going to position 2');
+
+				hm.setCurrentPosition(hm.getCurrentPosition() - 1);
+				assert.equal(hm.getCurrentPosition(), 1, 'going to position 1');
+				assert.deepEqual(hm.getCurrentState(), {
+					title: 'ls',
+					composer: 'hey'
+				});
+
+				hm.setCurrentPosition(hm.getCurrentPosition() - 1);
+				assert.equal(hm.getCurrentPosition(), 0, 'going to position -1');
+				assert.deepEqual(hm.getCurrentState(), {
+					title: 'ls'
+				});
+
 				hm.setCurrentPosition(hm.getCurrentPosition() + 3);
-				assert.deepEqual(hm.getCurrentState(), {title:'ls', composer:'hi'});
+				assert.deepEqual(hm.getCurrentState(), {
+					title: 'ls',
+					composer: 'huu'
+				});
 				assert.equal(hm.getCurrentPosition(), 3);
+
+				//REWRITING HISTORY: 
+				//	go back to state 1
+				hm.setCurrentPosition(hm.getCurrentPosition() - 2);
+				assert.equal(hm.getCurrentPosition(), 1);
+				assert.deepEqual(hm.getCurrentState(), {
+					title: 'ls',
+					composer: 'hey'
+				}, 'rewriting history');
+
+				//add new change
+				hm.addToHistory({
+					title: 'ls',
+					composer: '104'
+				}, 'changing composer');
+				assert.equal(hm.getCurrentPosition(), 2, 'tests 2nd change');
+				
+				assert.deepEqual(hm.getCurrentState(), {
+					title: 'ls',
+					composer: '104'
+				}, 'checking just inserted entry');
+
+				hm.setCurrentPosition(hm.getCurrentPosition() - 1);
+				assert.deepEqual(hm.getCurrentState(), {
+					title: 'ls',
+					composer: 'hey'
+				}, 'checking going back to entry' );
 
 			});
 		}
