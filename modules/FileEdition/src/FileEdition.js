53b10bb5efba044c42e147d7f743b262c4871a14
Vincent Degroote <vde@optimaweb.fr>
2016-05-24 11:19:22 +0200

    Remove all code related to save functionnality (#13)

--- modules/FileEdition/src/FileEdition.js
+++ modules/FileEdition/src/FileEdition.js
@@ -6,9 +6,9 @@ define([
 	 * FileEdition constructor
 	 * @exports FileEdition
 	 */
-	function FileEdition(songModel, viewer, saveFunction, params, saveAsF) {
-		this.view = new FileEditionView(params);
-		new FileEditionController(songModel, viewer, saveFunction, true); //saveAs is true
+	function FileEdition(songModel, viewer, viewParams) {
+		this.view = new FileEditionView(viewParams);
+		new FileEditionController(songModel, viewer); //saveAs is true
 	}
 	return FileEdition;
 });
--- modules/FileEdition/src/FileEditionController.js
+++ modules/FileEdition/src/FileEditionController.js
@@ -4,18 +4,14 @@ define([
 	'modules/converters/MusicCSLJson/src/SongModel_CSLJson',
 	'modules/converters/MusicXML/src/SongModel_MusicXML',
 	'modules/LSViewer/src/LSViewer',
-	'pubsub',
-	'utils/UserLog',
-	'utils/apiFlowMachines/ComposerServlet',
-	'utils/PopIn',
 	'jsPDF',
 	'jquery'
-], function(Mustache, SongModel, SongModel_CSLJson, SongModel_MusicXML,LSViewer, pubsub, UserLog, ComposerServlet, PopIn, jsPDF, $) {
+], function(Mustache, SongModel, SongModel_CSLJson, SongModel_MusicXML, LSViewer, jsPDF, $) {
 	/**
 	 * FileEditionController manages all file interaction like save, import, export
 	 * @exports FileEdition/FileEditionController
 	 */
-	function FileEditionController(songModel, viewer, saveFunction, saveAs) {
+	function FileEditionController(songModel, viewer) {
 		if (viewer) {
 			this.viewer = viewer;
 			if (viewer.canvas) {
@@ -24,13 +20,6 @@ define([
 		}
 		this.songModel = songModel || new SongModel();
 		this.initSubscribe();
-		var self = this;
-		if (saveAs){
-			this.initSubscribeSaveAS();
-		}
-		if (saveFunction) {
-			this.saveFn = saveFunction;
-		}
 	}
 
 	/**
@@ -44,43 +33,18 @@ define([
 		$.subscribe('FileEditionView-importMusicXML', function(el, musicXMLSong) {
 			self.importMusicXML(musicXMLSong);
 		});
-
-		$.subscribe('FileEditionView-exportPNG', function(el) {
-			self.exportPNG();
-		});
+		$.subscribe('FileEditionView-exportPNG', self.exportPNG);
 		$.subscribe('FileEditionView-exportPDF', function(el) {
-			self.exportAndPromptLeadsheetToPDF(self.songModel.getTitle(), self.songModel.getComposer(), self.songModel.getTimeSignature(), self.songModel.getStyle());
+			self.exportAndPromptLeadsheetToPDF(
+				self.songModel.getTitle(),
+				self.songModel.getComposer(),
+				self.songModel.getTimeSignature(),
+				self.songModel.getStyle()
+			);
 		});
-		$.subscribe('FileEditionView-exportMusicCSLJSON', function(el) {
-			self.exportLeadsheetJSON();
-		});
-		$.subscribe('FileEditionView-save', function(el) {
-			self.save();
-		});
-		$.subscribe('FileEditionView-saveAs', function(el) {
-			self.saveAs();
-		});
+		$.subscribe('FileEditionView-exportMusicCSLJSON', self.exportLeadsheetJSON);
 	};
-	//TODO: this logic is dependent on the application that uses LeadsheetJS, not on LeadsheetJS, maybe it should be moved, just like 'save' function
-	FileEditionController.prototype.initSubscribeSaveAS = function(first_argument) {	
-		var content = 	"<div class='controls form-inline'>"+
-						"<label style='padding:5px;'>New Title</label><input name='title' type='text'></input>"+
-						"&nbsp;&nbsp;<label style='padding:5px;'>Link to original lead sheet</label><input type='checkbox' name='linked' checked='true'/>"+
-						"</div>";
-		var classTitle = "saveAs";
-		this.popin = new PopIn('Save As', content, {classTitle:classTitle});
-		this.popin.render();
-		var self = this;
-		$(document).on('click','.'+ classTitle +' .modal_submit', function(data){
-			var title = $("input[name=title]").val();
-			title = title || 'Copy of ' + self.songModel.getTitle();
-			self.songModel.setTitle(title);
-			var linked = $("input[name='linked']")[0].checked;
-			self.save(self.songModel, false, linked);
-			$.publish('ToViewer-draw', self.songModel);
-		});
 
-	};
 
 	FileEditionController.prototype.importMusicCSLJSON = function(JSONSong) {
 		if (typeof JSONSong === "undefined") {
@@ -207,7 +171,7 @@ define([
 	
 	FileEditionController.prototype.exportLeadsheetJSON = function() {
 		var JSONSong = SongModel_CSLJson.exportToMusicCSLJSON(this.songModel);
-		// Code is a bit special for json because we transform data and we add a 'data:' prefix after href to make it works
+		// Code is a bit special for json because we transform data and we add a 'data:' prefix after href to make it work
 		var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(JSONSong));
 		var export_link = $('<a>', {
 			download: this.songModel.getTitle() + '.json',
@@ -216,42 +180,6 @@ define([
 		export_link[0].click();
 		export_link.remove();
 	};
-
-	FileEditionController.prototype.save = function(newLeadsheet, showConfirm, derived) {
-		
-		showConfirm = showConfirm !== undefined ? showConfirm : true;
-
-		var songId;
-		if (!newLeadsheet && this.songModel._id !== undefined) {
-			songId = this.songModel._id;
-		}
-		var derivedId = derived ? this.songModel._id : null;
-
-		this.songModel._id = undefined; // we need to clean songModel id otherwise update doesn't work
-		var JSONSong = SongModel_CSLJson.exportToMusicCSLJSON(this.songModel);
-
-		if (this.saveFn !== undefined) {
-			if (showConfirm && !confirm("Are you sure you want to save the changes in "+this.songModel.getTitle()+"?")){
-				return;
-			}
-			var self = this;
-			var idLog = UserLog.log('info', 'Saving...');
-			this.saveFn(JSONSong, songId, {derivedId: derivedId}, function(data) {
-				UserLog.removeLog(idLog);
-				if (data.error) {
-					UserLog.logAutoFade('error', data.msg);
-				} else {
-					UserLog.logAutoFade('success', 'Leadsheet saved with success');
-				}
-				self.songModel._id = data.id;
-			});
-		}
-	};
-
-	FileEditionController.prototype.saveAs = function() {
-		this.popin.show();
-	};
-
 
 	return FileEditionController;
 });
