4b5c8a7f3a2cb55567ab4a1ffca4bd33fd5e76be
Dani Martin <dmartinmartinez@gmail.com>
2016-06-10 10:56:54 +0200

    removed obsolete menu tests

--- modules/MainMenu/test/testMainMenuController.js
+++ modules/MainMenu/test/testMainMenuController.js
@@ -8,15 +8,9 @@ define([
 			test("MainMenuController", function(assert) {
 				var mmc = new MainMenuController();
 				assert.ok(mmc instanceof MainMenuController);
-
 				mmc.model.addMenu({title:'Notes', view: undefined});
 				mmc.model.addMenu({title:'Chords', view: undefined});
-				
-				mmc.activeMenu('Lyrics');
-				assert.equal(mmc.model.getCurrentMenu().title, 'Notes', "Try active menu on a menu that doesn't exist. Default is the first Menu.");
-
-				mmc.activeMenu('Chords');
-				assert.equal(mmc.model.getCurrentMenu().title, 'Chords', "try to activate a menu");
+			
 			});
 		}
 	};
